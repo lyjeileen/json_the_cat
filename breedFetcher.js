21eed311c77db997fc24302e5272a603d27d1bc1
lyjeileen <lyjeileen@gmail.com>
2022-09-30 05:50:22 +0000

    add test file

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,22 @@
-const args = process.argv.slice(2);
 const request = require("request");
-request(
-  `https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`,
-  (error, response, body) => {
-    if (error) {
-      console.log(error);
-      process.exit();
-    }
 
-    if (!body.includes(args[0])) {
-      console.log("Breed Not Found");
-      process.exit();
+const fetchBreedDescription = function (breedName, callback) {
+  request(
+    `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
+    (error, response, body) => {
+      if (error) {
+        callback(error);
+      } else {
+        if (!body.includes(breedName)) {
+          callback("Breed Not Found");
+        } else {
+          const data = JSON.parse(body);
+          const desc = data[0].description;
+          callback(error, desc);
+        }
+      }
     }
+  );
+};
 
-    const data = JSON.parse(body);
-    console.log(data[0]);
-  }
-);
+module.exports = { fetchBreedDescription };
